7da5174d8d7b1d54e669fa14a7944d6e27afae31
Tarlan Ismayilsoy <terlanismayilsoy@Tarlans-MacBook.local>
2020-02-12 15:13:07 +0400

    DHKeyExchanger updated

--- src/DHKeyExchanger.java
+++ src/DHKeyExchanger.java
@@ -1,5 +1,30 @@
+import java.util.Random;
 
 public class DHKeyExchanger
 {
+	private int p, alpha;
+	private int publicKey, privateKey;
+	private Random rnd = new Random();
+	
+	public DHKeyExchanger(int p, int alpha)
+	{
+		this.alpha = alpha;
+		this.p = p;
+		
+		privateKey = rnd.nextInt(p);
+		publicKey = (int) (Math.pow(alpha, privateKey) % p);
+		
+		System.out.println(privateKey + " " + publicKey);
+	}
+	
+	public int getSecretKey(int publicKey)
+	{
+		return (int) (Math.pow(publicKey, privateKey) % p);
+	}
+	
+	public int getPublicKey()
+	{
+		return publicKey;
+	}
 	
 }
--- src/MessageExchanger.java
+++ src/MessageExchanger.java
@@ -9,5 +9,15 @@ public class MessageExchanger
 		System.out.println(enc);
 		System.out.println(dec);
 		System.out.println("IV: " + AES.getInitVector());
+		
+		DHKeyExchanger dhAlice = new DHKeyExchanger(11, 2);
+		DHKeyExchanger dhBob = new DHKeyExchanger(11, 2);
+
+		
+		
+		int aliceKey = dhAlice.getSecretKey(dhBob.getPublicKey());
+		int bobKey = dhBob.getSecretKey(dhAlice.getPublicKey());
+		
+		System.out.println(aliceKey == bobKey);
 	}
 }
